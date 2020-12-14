f323ba2f223fb05835bfa21bbefb3effc3909032
pamahdion <parinaz.mahdion@aau.at>
2020-12-14 21:04:00 +0100

    add build

---
+++ Masked_Combined_AES/build/library/CMakeFiles/thumb-sim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/thumb-sim"
+  "../bin/thumb-sim.pdb"
+  "CMakeFiles/thumb-sim.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thumb-sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
